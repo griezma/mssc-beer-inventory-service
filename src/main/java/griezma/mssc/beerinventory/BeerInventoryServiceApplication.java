e44060396e87fc3170da89992c2a59a06a297e03
griezma <griezma@outlook.com>
2021-01-24 15:19:33 +0100

    config service config

--- src/main/java/griezma/mssc/beerinventory/BeerInventoryServiceApplication.java
+++ src/main/java/griezma/mssc/beerinventory/BeerInventoryServiceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BeerInventoryApplication {
+public class BeerInventoryServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BeerInventoryApplication.class, args);
+        SpringApplication.run(BeerInventoryServiceApplication.class, args);
     }
 }
